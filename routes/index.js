b424ba2c5effbc29c0ef7a37d45124f327a94004
skehdxhd96 <xman96@naver.com>
2021-05-02 20:27:10 +0900

    gitignore file add

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../config/database.js');
+var conn = db.init();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-/* GET test*/
-router.get('/test', function(req, res, next) {
-  res.send("test!!!")
+/* GET test + MySQL연동 test!!*/
+router.get('/fieldList', function(req, res, next) {
+  var sql = 'SELECT distinct CB_field FROM CompeteBoard';
+  conn.query(sql, function (err, rows, fields) {
+    if(err) console.log('query is not excuted. select fail...\n' + err);
+    else res.send(rows);
+  });
 });
 
 
