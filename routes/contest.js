71ea43f6e3ddb919efcba47dbc3edb409d947e9a
wodhks08 <ghks2047@naver.com>
2021-05-04 09:26:53 +0900

    contest

--- routes/contest.js
+++ routes/contest.js
@@ -3,6 +3,21 @@ var router = express.Router();
 var db = require('../config/database.js');
 var conn = db.init();
 
+
+function calc_end_date(row){
+  var now = new Date();
+  var date = row.CB_finalDate;
+  console.log(now)
+  console.log(date)
+  var date = date.split(/년|월|일|/);
+  //오전 오후 연산
+  var a = date[3].split(/:|오전|오후/)
+  if(date[3].inclues("오후")){
+    tmp.map(function(item){return parseInt(item, 10);})  
+  
+  }
+
+}
 /* GET test + MySQL연동 test!!*/
 router.get('/', function(req, res, next) {
   //모든 데이터를 최신순으로 20개만 가져온다.
@@ -11,7 +26,8 @@ router.get('/', function(req, res, next) {
     if(err) console.log('query is not excuted. select fail...\n' + err);
     else {
       //데이터의 마감 일을 계산한다.
-      console.log()
+      console.log(rows)
+      //calc_end_date(rows[0]);
       res.send(rows);
     }
   });
