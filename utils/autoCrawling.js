b48747763ec6b83eec786a66d1ece58f16df6a41
wodhks08 <ghks2047@naver.com>
2021-05-22 15:32:15 +0900

    change cron time every 24hour

--- utils/autoCrawling.js
+++ utils/autoCrawling.js
@@ -2,7 +2,7 @@ const cronJob = require('cron').CronJob;
 const TEST = require('./test')
 const Crawling = require('./Crawling')
 
-new cronJob('0 0 */1 * * *', () => {
+new cronJob('0 0 */24 * * *', () => {
     // 여기서 원하는 사용자 정의 함수
     console.log("Auto Crawling Start!@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
     Crawling()
