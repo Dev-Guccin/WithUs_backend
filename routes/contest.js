7518bff22576313025144b9ba7aff4cdf33b8349
wodhks08 <ghks2047@naver.com>
2021-05-27 17:13:59 +0900

    modify option search for pagination

--- routes/contest.js
+++ routes/contest.js
@@ -3,37 +3,6 @@ var router = express.Router();
 var db = require('../config/database.js');
 var conn = db.init();
 
-
-function calc_end_date(row){
-  var now = new Date();
-  var date = row.CB_finalDate;
-  console.log(now)
-  console.log(date)
-  var date = date.split(/년|월|일|/);
-  //오전 오후 연산
-  var a = date[3].split(/:|오전|오후/)
-  if(date[3].inclues("오후")){
-    tmp.map(function(item){return parseInt(item, 10);})  
-  
-  }
-
-}
-router.get('/:page', function(req, res, next) {
-  //페이지네이션
-  var page = (parseInt(req.params.page)-1) * 20;
-  //모든 데이터를 최신순으로 20개만 가져온다.
-  var sql = 'SELECT CB_code,CB_title,CB_field,CB_organization,CB_finalDate,CB_photo FROM withus.CompeteBoard order by CB_startDate desc limit 20 offset '+page+";";
-  conn.query(sql, function (err, rows, fields) {
-    if(err) console.log('query is not excuted. select fail...\n' + err);
-    else {
-      //데이터의 마감 일을 계산한다.
-      //console.log(rows)
-      //calc_end_date(rows[0]);
-      res.send(rows);
-    }
-  });
-});
-
 const fieldname = ["환경/에너지","콘텐츠/웹툰","취업/창업","음악/예술","연구/학술/논문","아이디어/기획",
 "사진/영상/UCC","문화/영화/문학","디자인/미술","네이밍/슬로건","금융/경제/경영","과학/공학/IT"]
 const targetname=["누구나지원", "청소년","대학(원)생", "취준생", "직장인"]
@@ -106,8 +75,26 @@ router.post('/options/:page', function(req, res, next) {
   conn.query(sql, function (err, rows, fields) {
     if(err) console.log('query is not excuted. select fail...\n' + err);
     else {
+      // 여기서 개수 구해야함.
       console.log(rows)
-      res.send(rows);
+      // 해당 조건의 개수를 구하기
+      var sql = `SELECT COUNT(*) FROM withus.CompeteBoard `
+      +`${(fieldstring.length > 0 || targetstring.length > 0 || keywordstring.length > 0)? "where ":""}`
+      +`${fieldstring}`
+      +`${(fieldstring.length > 0 && targetstring.length > 0 )? "and "+targetstring : targetstring}`
+      +`${((fieldstring.length > 0 || targetstring.length > 0) && keywordstring.length > 0)? "and "+keywordstring : keywordstring }`
+      +`;`;
+      console.log(sql)
+      conn.query(sql, rows, function (err, numrows, fields) {
+        if(err) console.log('query is not excuted. select fail...\n' + err);
+        else{
+          console.log(rows)
+          console.log(numrows)
+          rows.push({"count":numrows[0]["COUNT(*)"]})
+          console.log(rows)
+          res.send(rows);
+        }
+      })
     }
   });
 });
